caadf1b58f03b3db6f7ee96c7c36f19abce9fc95
Nischay Nahata <nischayn22@gmail.com>
2014-06-26 08:29:23 +0530

    Adds some effects
    When the bubbles are hovered they start moving out and on release go back
    to their location.

--- js/mediawiki.d3.js
+++ js/mediawiki.d3.js
@@ -4,10 +4,10 @@ function dv_d3_tree( element, i ){
 	var d3ID   = element.attr( 'id' ),
 	root = $.parseJSON( element.attr('dv_data') );
 
-	var diameter = 360;
+	var diameter = 440;
 
 	var tree = d3.layout.tree()
-		.size([360, diameter / 2 - 60]);
+		.size([360, 360 / 2 - 60]);
 
 	var svg = d3.select( "#" + d3ID ).append("svg")
 		.attr("width", diameter )
@@ -53,38 +53,54 @@ function dv_d3_tree( element, i ){
 			return 30 + 10*( size/maxSize );
 		});
 
-	var text = node.append("text")
-				.attr("transform", function(d) {  return "rotate(" + -(d.x - 180) + ")"; })
-				.style("pointer-events", "none")
-				.style("text-anchor", "middle")
-				.style("font", function(d){
-					return "10px sans-serif";
-				})
-				.attr("x", 0 )
-				.attr("y", 0 );
+	//node is actually an array of nodes but d3 takes care of everything
+	draw_node(node);
 
-	text.each(function(d){
-		var arr = [];
-		var name = d.name.replace(/_/g,' ');
-		var cur = 0, prev = 0;
-		while(cur < name.length){
-			cur += 10;
-			while(name[cur] != ' ' && cur < name.length) cur++;
-			arr.push(name.substr(prev, cur-prev+1));
-			prev = cur+1;
-		}
-		if (arr.length != 0) {
-			for (i = 0; i < arr.length; i++) {
-				d3.select(this).append("tspan")
-					.text(arr[i])
-					.attr("dy", i ? "1.2em" : 0)
-					.attr("x", 0)
-					.attr("text-anchor", "middle")
-					.attr("class", "tspan" + i);
+	// to make d a d3 object need to do select(this)
+	node.each(function(d) { d = d3.select(this); d.on("mouseover", function(){
+			d.transition().attr("transform", function(d) { return "rotate(" + (d.x - 180) + ")translate(" + 160 + ")"; }).duration(2000);
+		})
+	})
+
+	node.each(function(d) { d = d3.select(this); d.on("mouseout", function(){
+			d.transition().attr("transform", function(d) { return "rotate(" + (d.x - 180) + ")translate(" + d.y + ")"; }).delay(100);
+		})
+	})
+
+	function draw_node( node ){
+		var text = node.append("text")
+					.attr("transform", function(d) {  return "rotate(" + -(d.x - 180) + ")"; })
+					.style("pointer-events", "none")
+					.style("text-anchor", "middle")
+					.style("font", function(d){
+						return "10px sans-serif";
+					})
+					.attr("x", 0 )
+					.attr("y", 0 );
+
+		text.each(function(d){
+			var arr = [];
+			var name = d.name.replace(/_/g,' ');
+			var cur = 0, prev = 0;
+			while(cur < name.length){
+				cur += 10;
+				while(name[cur] != ' ' && cur < name.length) cur++;
+				arr.push(name.substr(prev, cur-prev+1));
+				prev = cur+1;
 			}
-			d3.select(this).attr("y", -3 * (arr.length - 1) );
-		}
-	});
+			if (arr.length != 0) {
+				for (i = 0; i < arr.length; i++) {
+					d3.select(this).append("tspan")
+						.text(arr[i])
+						.attr("dy", i ? "1.2em" : 0)
+						.attr("x", 0)
+						.attr("text-anchor", "middle")
+						.attr("class", "tspan" + i);
+				}
+				d3.select(this).attr("y", -3 * (arr.length - 1) );
+			}
+		});
+	}
 
 	d3.select(self.frameElement).style("height", diameter - 150 + "px");
 };
